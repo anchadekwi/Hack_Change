f88d3ba3b2f91b50137cc2fc22eb3179c0de303d
kostanikonov <kostyanikonov07@gmail.com>
2025-11-30 10:21:09 +0300

    add commentaries

--- pipeline.py
+++ pipeline.py
@@ -6,6 +6,7 @@ from data_collector import YoutubeClient
 mws_client = MWSClient(
     "https://tables.mws.ru/fusion/v1/datasheets/dstEPg0bL9lD8jiDmt",
     "uskPUFZhMwASVADEGwgI4XN",
+    "https://tables.mws.ru/fusion/v1/datasheets/dstCV00pr7W11osgz5"
 )
 
 
@@ -18,16 +19,59 @@ async def main():
         "https://vk.com/rdrc_ru?from=groups",
     )
     yt_parser = YoutubeClient(
-        "AIzaSyDIUvl2iJXBlDS0D1upZnb7azuzFfPq-hY",
+        "AIzaSyCdrOUYNbVUr3nmZARZ2dEOE8s8WzsVOSM",
         "https://www.youtube.com/@RollAllDay",
     )
     await telegram_parser.authenticate()
+
+    existing_post_uris = mws_client.fetch_post_uris()
+
     posts_tg = await telegram_parser.get_posts_info()
     posts_vk = vk_parser.get_posts_info()
     posts_yt = yt_parser.get_videos_info()
-    mws_client.insert_rows([i | {"source": "Телеграм"} for i in posts_tg])
-    mws_client.insert_rows([i | {"source": "ВК"} for i in posts_vk])
-    mws_client.insert_rows([i | {"source": "Ютуб"} for i in posts_yt])
+    new_tg_rows = [
+        i | {"source": "Телеграм"}
+        for i in posts_tg
+        if i["uri"] not in existing_post_uris
+    ]
+    print("TG new rows:", len(new_tg_rows))
+    mws_client.insert_rows(new_tg_rows)
+    new_vk_rows = [
+        i | {"source": "ВК"} for i in posts_vk if i["uri"] not in existing_post_uris
+    ]
+    print("VK new rows:", len(new_vk_rows))
+    mws_client.insert_rows(new_vk_rows)
+
+    new_yt_rows = [
+        i | {"source": "Ютуб"} for i in posts_yt if i["uri"] not in existing_post_uris
+    ]
+    print("YT new rows:", len(new_yt_rows))
+    mws_client.insert_rows(new_yt_rows)
+
+    existing_comments_uris = mws_client.fetch_comments_uris()
+
+    comments_tg = await telegram_parser.get_comments_for_post("https://t.me/shucarz1337/771")
+    comments_vk = vk_parser.get_comments_from_post("https://vk.com/wall-69473024_124934")
+    comments_yt = yt_parser.get_comments_for_video("https://www.youtube.com/watch?v=UyaoBy3ETYI")
+
+    new_tg_rows = [
+        i | {"source": "Телеграм"}
+        for i in comments_tg
+        if i["text"] not in existing_comments_uris
+    ]
+    print("TG new comments:", len(new_tg_rows))
+    mws_client.insert_rows_for_comments(new_tg_rows)
+    new_vk_rows = [
+        i | {"source": "ВК"} for i in comments_vk if i["text"] not in existing_comments_uris
+    ]
+    print("VK new comments:", len(new_vk_rows))
+    mws_client.insert_rows_for_comments(new_vk_rows)
+
+    new_yt_rows = [
+        i | {"source": "Ютуб"} for i in comments_yt if i["text"] not in existing_comments_uris
+    ]
+    print("YT new comments:", len(new_yt_rows))
+    mws_client.insert_rows_for_comments(new_yt_rows)
 
 
 if __name__ == "__main__":
